bb74348ae4558fff9695e7d227f092d1f0963de3
cflass1 <cflass@gmail.com>
2022-03-11 19:35:48 -0700

    Add project files.

---
+++ CodingChallengeCF2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingChallengeCF2.MvcApplication" Language="C#" %>
---
+++ CodingChallengeCF2/Models/Thing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodingChallengeCF2.Models
+{
+    public class Thing
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Planet")]
+        public string Planet { get; set; }
+        [Display(Name = "Hazard")]
+        public string Hazard { get; set; }
+
+    }
+}
